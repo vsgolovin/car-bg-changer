7eb223077fb34bdd2017995c4dd31099e3d95c75
Vyacheslav Golovin <v.s.golovin@gmail.com>
2023-06-13 09:51:01 +0300

    Move executable files to root

--- blend_images.sh
+++ blend_images.sh
@@ -1,4 +1,4 @@
-python3 src/insert_image.py \
+python3 insert_image.py \
     data/fg_images/photo_2023-06-06_08-59-59.jpg \
     data/masks_processed/photo_2023-06-06_08-59-59.png \
     data/bg_images/background.jpg \
